a1776aecb3f6f9e0f98a5b7fc357405da257d497
ckiw <alexis@ckiw.me>
2013-12-11 17:14:10 +0100

    dynamic field for disciplines

---
+++ DisciplineManager.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+try {
+// Nouvel objet de base SQLite 
+    $bdd = new PDO('sqlite:bdd.sqlite');
+// Quelques options
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if (isset($_GET['action'])) {
+        switch ($_GET['action']) {
+
+            case 'get':
+                $query = $bdd->query('SELECT id as idDiscipline, name FROM Discipline');
+                $disciplineList = [['label'=>"Disciplines","disciplines"=>[]]];
+                while ($data = $query->fetch()) {
+                    $disciplineList[0]["disciplines"][]= ["id"=>$data["id"],"name"=>$data["name"]];
+
+                    //var list = [{label:"Carabine",disciplines:{name:,id:}, 1, 5];
+                }
+                $query->closeCursor();
+                echo json_encode($disciplineList);
+                break;
+        }
+        
+    }
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+?>
