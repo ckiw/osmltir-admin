4022669bf01b1f2e0fe8d4dd12c8f1ce3a27841c
dagosml <da.ghesquiere@gmail.com>
2013-12-12 21:28:50 +0100

    Modification osmltir.fr/blog

--- js/appli.js
+++ js/appli.js
@@ -4,11 +4,11 @@ function modifNavbar() {
     //modification de la navbar en fonction de l'état de la connection
     $.getJSON('login.php', {action: 'isConnected'}, function(rep) {
         if (rep.grade == 'user') {
-            $('#navbar-content').append('<ul class="nav"><li><a href="index.html">Accueil</a></li><li><a href="gallerie.html">Gallerie</a></li><li><a href="score.html">Score</a></li></ul><ul  class="nav pull-right"><li ><a href="user.html">' + rep.name + '</a></li><li ><a href="#" id="disconnect">&times;</a></li></ul>');
+            $('#navbar-content').append('<ul class="nav"><li><a href="index.html">Accueil</a></li><li><a href="galerie.html">Galerie</a></li><li><a href="http://www.osmltir.fr/blog/">Blog</a></li><li><a href="score.html">Score</a></li></ul><ul  class="nav pull-right"><li ><a href="user.html">' + rep.name + '</a></li><li ><a href="#" id="disconnect">&times;</a></li></ul>');
         } else if (rep.grade == 'admin') {
-            $('#navbar-content').append('<ul class="nav"><li><a href="index.html">Accueil</a></li><li><a href="gallerie.html">Gallerie</a></li><li><a href="score.html">Score</a></li></ul><ul  class="nav pull-right"><li ><a href="user.html">' + rep.name + '</a></li><li ><a href="admin.html">Admin</a></li><li ><a href="#" id="disconnect">&times;</a></li></ul>');
+            $('#navbar-content').append('<ul class="nav"><li><a href="index.html">Accueil</a></li><li><a href="galerie.html">Galerie</a></li><li><a href="http://www.osmltir.fr/blog/">Blog</a></li><li><a href="score.html">Score</a></li></ul><ul  class="nav pull-right"><li ><a href="user.html">' + rep.name + '</a></li><li ><a href="admin.html">Admin</a></li><li ><a href="#" id="disconnect">&times;</a></li></ul>');
         } else {
-            $('#navbar-content').append('<ul class="nav"><li><a href="index.html">Accueil</a></li><li><a href="gallerie.html">Gallerie</a></li></ul><ul class="nav pull-right"><li><a href="sign.html">Inscription</a></li></ul><form method="get" action="login.php" class="navbar-form pull-right"><input name="action" value="connection" type="hidden"\><input name="email" type="text" class="span2" placeholder="Email"\><input name="password" type="password" class="span2" placeholder="Mot de passe"\><button type="submit" class="btn">Se connecter</button></form>');
+            $('#navbar-content').append('<ul class="nav"><li><a href="index.html">Accueil</a></li><li><a href="galerie.html">Galerie</a></li><li><a href="http://www.osmltir.fr/blog/">Blog</a></li></ul><form method="get" action="login.php" class="navbar-form pull-right"><input name="action" value="connection" type="hidden"\><input name="email" type="text" class="span2" placeholder="Email"\><input name="password" type="password" class="span2" placeholder="Mot de passe"\><button type="submit" class="btn">Se connecter</button></form>');
         }
         $('#disconnect').click(function(e) {
             e.preventDefault();
